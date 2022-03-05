75bf8cb41872ff71bc374b190353080bc9544136
Nikih449 <druzhevskii@gmail.com>
2022-03-05 20:44:10 +0200

    message fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,7 @@ const auth = (req, res, next) => {
       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
     );
   } catch (err) {
-    throw new WrongLoginData('Требуется авторизация.');
+    throw new WrongLoginData('Требуется авторизация!');
   }
   req.user = payload;
   next();
